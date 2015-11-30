499f0bde034bf784308c5c70b2b04eb86e069ead
bamboo <jinrizhushi>
2015-11-30 17:39:49 +0800

    Add:add the stock activity

---
+++ app/src/main/java/cn/com/jinrizhushi/stock/stock/view/activity/StockActivity.java
@@ -0,0 +1,17 @@
+package cn.com.jinrizhushi.stock.stock.view.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * 描述: 股票的界面
+ * 作者: 刘倩
+ * 日期: 15/11/30 17:38
+ */
+public class StockActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+    }
+}
