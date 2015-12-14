e4836fae2e862f26bb6604d0dddbf71727920da9
bamboo <jinrizhushi>
2015-12-15 04:33:02 +0800

    Add:add the customScrollView

--- app/src/main/java/cn/com/jinrizhushi/stock/stock/view/activity/StockDetialActivity.java
+++ app/src/main/java/cn/com/jinrizhushi/stock/stock/view/activity/StockDetialActivity.java
@@ -1,13 +1,18 @@
 package cn.com.jinrizhushi.stock.stock.view.activity;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
+import org.androidannotations.annotations.AfterExtras;
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
 
 import cn.com.jinrizhushi.stock.R;
 import cn.com.jinrizhushi.stock.container.BaseActivity;
 import cn.com.jinrizhushi.stock.container.Constant;
 import cn.com.jinrizhushi.stock.stock.viewmodel.StockDetailViewModel;
+import cn.com.jinrizhushi.stock.util.customnormalview.CustomScrollView;
 
 /**
  * 描述: 股票详情页
@@ -15,15 +20,37 @@ import cn.com.jinrizhushi.stock.stock.viewmodel.StockDetailViewModel;
  * 日期: 15/12/11 14:32
  */
 @EActivity
-public class StockDetialActivity extends BaseActivity{
+public class StockDetialActivity extends BaseActivity implements CustomScrollView.OnScrollListener {
     StockDetailViewModel model;
+    @ViewById(R.id.ll_news_top)
+    LinearLayout llTop;
+    @ViewById(R.id.ll_news_middle)
+    LinearLayout llMiddle;
+    @ViewById(R.id.sv_custom)
+    CustomScrollView scrollView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        Bundle bundle = this.getIntent().getExtras();
 //        String stockId = bundle.getString(Constant.STOCK_ID);
         String stockId = "111";
-        model = new StockDetailViewModel(this,stockId);
-        initializeContentView(R.layout.activity_stock_detail,model);
+        model = new StockDetailViewModel(this, stockId);
+        initializeContentView(R.layout.activity_stock_detail, model);
+    }
+    @AfterViews
+    void setData() {
+        scrollView.setOnScrollListener(this);
+    }
+
+    /**
+     * 回调方法， 返回CustomScrollView滑动的Y方向距离
+     *
+     * @param scrollY
+     */
+    @Override
+    public void onScroll(int scrollY) {
+        int mBuyLayout2ParentTop = Math.max(scrollY, llMiddle.getTop());
+        llTop.layout(0, mBuyLayout2ParentTop, llTop.getWidth(), mBuyLayout2ParentTop + llTop.getHeight());
+
     }
 }
